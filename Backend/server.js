77998072192879bb2de9d3e61e09be22bef972a5
Pau Franch <pfranch08@gmail.com>
2024-12-14 19:45:28 +0900

    Backend que no fa pena

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,16 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
+const app = express();
+const PORT = 3000;
 
-//mongoose.connect('mongodb://localhost:27017')
+mongoose.connect('mongodb://127.0.0.1:27017/concesionario')
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch((error) => console.error('Error al conectar a MongoDB', error));
 
 app.get('/', (req, res) => {
-    res.send('<hl>Hello, Express.js Server! 8==========D</h1>');
+    res.send('Hello world!');
 });
 
-const port = process.env.PORT || 27017;
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
-
