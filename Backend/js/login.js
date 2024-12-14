a8fed7eaf07c398d53d4978e32261055352f871c
Max Vilà Ruiz <max16506@gmail.com>
2024-12-14 16:30:53 +0100

    Backend: Log In Data

---
+++ Backend/js/login.js
@@ -0,0 +1,8 @@
+function login() {
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    // window.location.href = "/Frontend/html/userpage.html";
+
+    console.log(email.value);
+    console.log(password.value);
+}
