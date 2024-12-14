f85a6caa91e62ead7634a7d5764979c28e19ed00
Pau Franch <pfranch08@gmail.com>
2024-12-15 00:00:32 +0900

    Backend bastant be

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const app = express();
 const PORT = 3000;
 
-mongoose.connect('mongodb://127.0.0.1:27017/concesionario')
+mongoose.connect('mongodb://127.0.0.1:27017/auth')
   .then(() => console.log('Conectado a MongoDB'))
   .catch((error) => console.error('Error al conectar a MongoDB', error));
 
@@ -22,7 +23,7 @@ const User = mongoose.model('User', userSchema);
 app.use(express.json());
   
 const verifyToken = (req, res, next) => {
-  const token = req.headers['authoritzation'];
+  const token = req.headers['authorization'];
   if(!token)
   {
     return res.status(401).json({ error: 'Unauthorized' });
@@ -31,13 +32,14 @@ const verifyToken = (req, res, next) => {
   jwt.verify(token, 'secret', (err, decoded) => {
   if(err)
   {
-    return res.status(401).json({ error: 'Unauthorized' });
+    return res.status(401).json({ error: err.message });
   }
     req.user = decoded;
     next();
   });
 };
 
+// REGISTER
 app.post('/api/register', async (req, res) => {
   try {
     const existingUser = await User.findOne({ username: req.body.username });
@@ -56,6 +58,41 @@ app.post('/api/register', async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error){
+    res.status(500).json({ error: toString(error) });
+  }
+});
+
+// LOGIN
+app.post('/', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if(!user)
+    {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    const passwordMatch = await bcrypt.compare(req.body.password, user.password);
+    if(!passwordMatch)
+    {
+      return res.status(401).json({ error: 'Password does not match!' });
+    }
+
+    const token = jwt.sign({ username: user.username }, 'secret');
+    res.status(200).json({ token });
+  } catch (error){
+    res.status(500).json({ error: 'Internal server error.' });
+  }
+});
+
+//GET USER DETAILS
+app.get('/api/user', verifyToken, async (req, res) => {
+  try {
+    const user = await User.findOne( { username: req.user.username });
+    if(!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json({ username: user.username });
+  } catch {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
